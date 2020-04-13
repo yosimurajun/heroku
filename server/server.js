6b9de868687746e0bf06a1d71f35fb9896fca586
yosimurajun <gksrnrdkssud@gmail.com>
2020-04-13 18:11:35 +0900

    수정

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const publicPath = path.join(__dirname, '..', 'public');
+const publicPath = path.join(__dirname, '..', '/public');
 const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
@@ -9,5 +9,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(publicPath, 'index.html'));
 });
 app.listen(port, () => {
+  console.log(publicPath);
   console.log('Server is up!');
 });
